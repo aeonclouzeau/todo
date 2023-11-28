7a642e9a89527f7e609fd28b528ed6033f5d13ad
Ricardo Feregrino <ricardo@jekuari.mx>
2023-11-28 12:00:58 -0600

    Fixed issue with todos

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,36 +4,35 @@ import Task from "./Task/Task";
 import axios from "axios";
 
 function Home() {
-	const [todos, setTodo] = useState([]);
-	useEffect(() => {
-		console.log("Before API call: " + todos);
-		axios
-			.get("http://localhost:3001/get")
-			.then((result) => {
-				setTodo(result.data);
-				console.log("After API call" + todos);
-			})
-			.catch((err) => console.warn(err));
-	}, []);
-	return (
-		<div>
-			<h1>To do</h1>
-			<Task />
+  const [todos, setTodo] = useState([]);
+  useEffect(() => {
+    console.log("Before API call: " + todos);
+    axios
+      .get("http://localhost:3001/get")
+      .then((result) => {
+        setTodo(result.data);
+        console.log("After API call" + todos);
+      })
+      .catch((err) => console.warn(err));
+  }, []);
+  return (
+    <div>
+      <h1>To do</h1>
+      <Task />
 
-			{todos.length === 0 ? (
-				<div>
-					<h3>No tasks added</h3>
-				</div>
-			) : (
-				todos.map((todo) => {
-					<div>
-						<p>{todo.task}</p>
-					</div>;
-					console.log(todos);
-				})
-			)}
-		</div>
-	);
+      {todos.length === 0 ? (
+        <div>
+          <h3>No tasks added</h3>
+        </div>
+      ) : (
+        todos.map((todo, index) => (
+          <div key={index}>
+            <p>{todo.task}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default Home;
